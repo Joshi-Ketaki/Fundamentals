25dfffc6619291e0142d5e9e48066354d5d7631b
Ketaki Rajiv Joshi <ketaki@mac.home>
2025-11-02 16:12:58 -0500

    Reorganize functionality

--- PerformancePlayground/mySoftmax.cu
+++ PerformancePlayground/mySoftmax.cu
@@ -1,16 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 
-void softmax_cpu(float *input, float *output, int N)
+float find_max(float *input, int N, float max_val)
 {
-    float max_val = -INFINITY;
-    // Find the maximum value for numerical stability
-    for(int i = 0; i < N; i++)
+    max_val = input[0];
+    for(int i = 1; i < N; i++)
     {
         if(input[i] > max_val)
             max_val = input[i];
     }
-
+    return max_val;
+}
+void softmax_cpu(float *input, float *output, int N, int max_val)
+{
     //Compute the exponentials and also the sum
     float sum = 0.0f;
     for(int i = 0; i < N; i++)
@@ -30,6 +32,7 @@ void softmax_cpu(float *input, float *output, int N)
 int main()
 {
     int N = 4;
+    float max_val;
     float *input = (float*) malloc(sizeof(float) * N);
     float *output = (float*) malloc(sizeof(float) * N);
     // Initialize input with some values
@@ -38,8 +41,10 @@ int main()
         input[i] = 1.0f * (rand() % 10);
     }
 
+    // Find max value in input
+    max_val = find_max(input, N, max_val);
     // Compute softmax on CPU
-    softmax_cpu(input, output, N);
+    softmax_cpu(input, output, N, max_val);
 
     for(int i = 0; i < N; i++)
     {
