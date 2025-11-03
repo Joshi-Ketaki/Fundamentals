16afa4ebdce50f357fc8d9623794879484bc05ed
Ketaki Rajiv Joshi <ketaki@mac.home>
2025-11-03 17:54:11 -0500

    leetgpu passed version

--- PerformancePlayground/mySoftmax.cu
+++ PerformancePlayground/mySoftmax.cu
@@ -126,61 +126,44 @@ __global__ void softmax_kernel_partial_max(float *d_input, float *d_blockMax, in
     __syncthreads(); 
     // Copy to global memory
    if (threadIdx.x == 0) d_blockMax[blockIdx.x] = *block_max;
+
 }
-__global__ void softmax_kernel_global_max(float* d_blockMax, float* globalMax, int numBlocks)
+float softmax_global_max(float* blockMax, int numBlocks)
 {
-    // Compute the max value in the block
-    if(threadIdx.x == 0)
+    float max_val = blockMax[0];
+    // bounds should be within the block && within N -- a block could go beyond N. think 1025
+    for(int i = 0; i < numBlocks; i++)
     {
-        float max_val = d_blockMax[0];
-        // bounds should be within the block && within N -- a block could go beyond N. think 1025
-        for(int i = 0; i < numBlocks; i++)
-        {
-            if(d_blockMax[i] > max_val)
-                max_val = d_blockMax[i];
-        }
-        // Copy to global memory
-        *globalMax = max_val;
-    } 
+        if(blockMax[i] > max_val)
+            max_val = blockMax[i];
+    }
+    // Copy to global memory
+    return max_val;
 }
 
 __global__ void softmax_kernel_partial_exp(float*d_input, float *globalMax, float* d_output, int N)
 {
-    extern __shared__ float sh_mem[];
-    float* sh_input = sh_mem;
-    float* gMax = sh_mem + blockDim.x;
-
     int idx = blockDim.x * blockIdx.x + threadIdx.x;
-
-    // Load the input
     if(idx < N)
-        sh_input[threadIdx.x] = d_input[idx];
-    else
-        sh_input[threadIdx.x] = -INFINITY;
-    
-    if(threadIdx.x == 0)
-        *gMax = *globalMax;
-    __syncthreads();
-
-
-    // Calculate the exponential
-    if(idx < N)
-        d_output[idx] = expf(sh_input[threadIdx.x] - *gMax);
+        d_output[idx] = expf(d_input[idx] - *globalMax);
 }
 
-__global__ void softmax_kernel_normalize(float* d_output, int N)
+__global__ void softmax_normalize(float* output, int N, float* sum)
 {
-    float sum = 0.0f;
-    for (int i = 0; i < N; i++)
-        sum += d_output[i];
-    for (int i = 0; i < N; i++)
-        d_output[i] /= sum;
+    //float sum = 0.0f;
+    //for (int i = 0; i < N; i++)
+    //    sum += output[i];
+    //for (int i = 0; i < N; i++)
+    //    output[i] /= sum;
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    if(idx < N)
+        output[idx] = output[idx]/ *sum;
 }
 
 int main()
 {
-    int N = 4;
-    float max_val;
+    int N = 33;
+    float max_val, *temp;
     float *input = (float*) malloc(sizeof(float) * N);
     float *cpu_output = (float*) malloc(sizeof(float) * N);
     float *gpu_output = (float*) malloc(sizeof(float) * N);
@@ -203,18 +186,28 @@ int main()
     cudaMalloc(&d_output, sizeof(float) * N);
     cudaMalloc(&d_blockMax, sizeof(float) * gridSize);
     cudaMalloc(&globalMax, sizeof(float));
-
+    cudaMalloc(&temp, sizeof(float));
+    float *blockMax = (float*) malloc(sizeof(float) * gridSize);
+    float *blockSum = (float*) malloc(sizeof(float) * gridSize);
+    for (int i = 0; i < gridSize; i++) blockSum[i] = 0.0f;
     cudaMemcpy(d_input, input, sizeof(float) * N, cudaMemcpyHostToDevice);
     //softmax_kernel_simple<<<1, N, (N + 2) * sizeof(float)>>>(d_input, d_output, N);
     softmax_kernel_partial_max<<<gridSize, blockSize, sizeof(float) * (blockSize + 1)>>>(d_input, d_blockMax, N);
-    softmax_kernel_global_max<<<1, 1>>>(d_blockMax, globalMax, gridSize);
+    cudaDeviceSynchronize();
+    cudaMemcpy(blockMax, d_blockMax, sizeof(float) * gridSize, cudaMemcpyDeviceToHost);
+    max_val = softmax_global_max(blockMax, gridSize);
+    cudaMemcpy(globalMax, &max_val, sizeof(float), cudaMemcpyHostToDevice);
     softmax_kernel_partial_exp<<<gridSize, blockSize, sizeof(float) * (blockSize + 1)>>>(d_input, globalMax, d_output, N);
-    softmax_kernel_normalize<<<1, 1>>>(d_output, N);
     cudaDeviceSynchronize();
     cudaMemcpy(gpu_output, d_output, sizeof(float) * N, cudaMemcpyDeviceToHost);
-
+    float sum = 0.0f;
+    for(int i = 0; i < N; i++) 
+        sum += gpu_output[i];
+    cudaMemcpy(temp, &sum, sizeof(float), cudaMemcpyHostToDevice);
+    softmax_normalize<<<gridSize, blockSize>>>(d_output, N, temp);
+    cudaMemcpy(gpu_output, d_output, sizeof(float) * N, cudaMemcpyDeviceToHost);
     // Validate outputs
-    validate_outputs(cpu_output, gpu_output, N);
+    validate_outputs(cpu_output, gpu_output, 5);
 
     free(input);
     free(cpu_output);
