9582ff1684445f1551fb8d64593ae679557e19b1
Ketaki Rajiv Joshi <ketaki@Ketakis-MacBook-Air.local>
2025-11-03 22:37:06 -0500

    unoptimized layerNorm

---
+++ PerformancePlayground/myLayerNorm.cu
@@ -0,0 +1,169 @@
+#include<stdio.h>
+#include<math.h>
+
+
+void initMat(float* A, int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        A[i] = rand() % 10;
+    }
+}
+
+void initMatConstantVal(float* A, int size, float val)
+{
+    for(int i = 0; i < size; i++)
+    {
+        A[i] = val;
+    }
+}
+
+void validate_outputs(float *cpu_output, float* gpu_output, int num_samples)
+{
+    printf("\nValidating first %d output elements...\n", num_samples);
+    for (int s = 0; s < num_samples; s++)
+    {
+        int idx = rand() % num_samples;
+        if(fabs(cpu_output[idx] - gpu_output[idx]) > 1e-5)
+        {
+            printf("\n Output mismatch!");
+            return;
+        }
+        printf("\n Outputs match");
+        printf("\n CPU Output[%d] = %.3f\n", idx,  cpu_output[idx]);
+        printf("\n GPU Output[%d] = %.3f\n", idx, gpu_output[idx]);
+    }
+    printf("\n(If these values look finite and not NaN/Inf, indexing is likely correct.)\n");
+}
+
+void layerNorm_cpu(float* X, float* Y, float* G, float* B, int batch_size, int feature_size)
+{
+    float var, epsilon = 1e-5;
+    for(int batch = 0; batch < batch_size; batch++)
+    {
+        // calculate mean
+        float mean = 0.0f;
+        for(int feature = 0; feature < feature_size; feature++)
+        {
+            mean += X[batch * feature_size + feature];
+        }
+        mean = mean / feature_size;
+
+        // calculate variation
+        float sum_diff = 0.0f, diff;
+        for(int feature = 0; feature < feature_size; feature++)
+        {
+            diff = X[batch * feature_size + feature] - mean;
+            sum_diff += (diff * diff);
+        }
+        var = sum_diff / feature_size;
+
+        //calculate x_hat
+        for(int feature = 0; feature < feature_size; feature++)
+        {
+            float x_hat = (X[batch * feature_size + feature] - mean) / sqrtf(var + epsilon);
+            float g = G ? G[feature] : 1.0f;
+            float b = B ? B[feature] : 0.0f;
+            Y[batch * feature_size + feature] = x_hat * g + b;
+        }
+    }
+}
+
+void calcMeanAndVar(float* X, float* allMean, float* allVar, int batch_size, int feature_size)
+{
+    for(int batch = 0; batch < batch_size; batch++)
+    {
+        // calculate mean
+        float mean = 0.0f;
+        for(int feature = 0; feature < feature_size; feature++)
+        {
+            mean += X[batch * feature_size + feature];
+        }
+        allMean[batch] = mean / feature_size;
+
+        // calculate variation
+        float sum_diff = 0.0f, diff;
+        for(int feature = 0; feature < feature_size; feature++)
+        {
+            diff = X[batch * feature_size + feature] - allMean[batch];
+            sum_diff += (diff * diff);
+        }
+        allVar[batch] = sum_diff / feature_size;
+    }
+}
+
+__global__ void layerNorm_kernel(float* X, float* Y, float* G, float* B, float* allMean, float* allVar, int batch_size, int feature_size)
+{
+    int batch = blockDim.y * blockIdx.y + threadIdx.y;
+    int feature = blockDim.x * blockIdx.x + threadIdx.x;
+    float epsilon = 1e-5;
+    if(batch < batch_size && feature < feature_size)
+    {
+        float x_hat = (X[batch * feature_size + feature] - allMean[batch]) / sqrtf(allVar[batch] + epsilon);
+        float g = G ? G[feature] : 1.0f;
+        float b = B ? B[feature] : 0.0f;
+        Y[batch * feature_size + feature] = x_hat * g + b;
+    }
+
+}
+
+int main()
+{
+    // say batch size is 5 and feature size is 4
+    // because leetgpu will not allow command line arg
+    int batch_size = 5, feature_size = 4;
+    int matSize = batch_size * feature_size;
+    float *X = (float*)malloc(sizeof(float) * matSize);
+    float *cpu_Y = (float*)malloc(sizeof(float) * matSize);
+    float *gpu_Y = (float*)malloc(sizeof(float) * matSize);
+    float *G = (float*)malloc(sizeof(float) * feature_size);
+    float *B = (float*)malloc(sizeof(float) * feature_size);
+
+    initMat(X, matSize);
+    initMatConstantVal(G, feature_size, 1.0f);
+    initMatConstantVal(B, feature_size, 0.0f);
+    
+    layerNorm_cpu(X, cpu_Y, G, B, batch_size, feature_size);
+
+    float *allMean = (float*)malloc(sizeof(float) * batch_size);
+    float *allVar = (float*)malloc(sizeof(float) * batch_size);
+    calcMeanAndVar(X, allMean, allVar, batch_size, feature_size);
+
+    float *d_X, *d_Y, *d_G, *d_B, *d_allMean, *d_allVar;
+    cudaMalloc(&d_X, sizeof(float) * matSize);
+    cudaMalloc(&d_Y, sizeof(float) * matSize);
+    cudaMalloc(&d_G, sizeof(float) * feature_size);
+    cudaMalloc(&d_B, sizeof(float) * feature_size);
+    cudaMalloc(&d_allMean, sizeof(float) * batch_size);
+    cudaMalloc(&d_allVar, sizeof(float) * batch_size);
+
+    cudaMemcpy(d_X, X, sizeof(float) * matSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_G, G, sizeof(float) * feature_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_B, B, sizeof(float) * feature_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_allMean, allMean, sizeof(float) * batch_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_allVar, allVar, sizeof(float) * batch_size, cudaMemcpyHostToDevice);
+
+    dim3 blockSize(32,32,1);
+    dim3 gridSize((feature_size + blockSize.x - 1)/blockSize.x,
+                  (batch_size + blockSize.y - 1)/blockSize.y, 1);
+    layerNorm_kernel<<<gridSize, blockSize>>>(d_X, d_Y, d_G, d_B, d_allMean, d_allVar, batch_size, feature_size);
+    cudaMemcpy(gpu_Y, d_Y, sizeof(float) * matSize, cudaMemcpyDeviceToHost);
+    validate_outputs(cpu_Y, gpu_Y, 5);
+
+    cudaFree(d_X);
+    cudaFree(d_Y);
+    cudaFree(d_G);
+    cudaFree(d_B);
+    cudaFree(d_allMean);
+    cudaFree(d_allVar);
+
+    free(X);
+    free(cpu_Y);
+    free(gpu_Y);
+    free(G);
+    free(B);
+    free(allMean);
+    free(allVar);
+
+    return 0;
+}
